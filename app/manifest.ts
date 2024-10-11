7e455594f97596b89869526d8e0f083f10e34872
lnkkerst <lnkkerst04@gmail.com>
2024-10-11 17:05:08 +0800

    feat: update icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,5 +6,17 @@ export default function manifest(): MetadataRoute.Manifest {
     description: "获取校园汇生活取水条码",
     start_url: "/",
     display: "standalone",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
   };
 }
